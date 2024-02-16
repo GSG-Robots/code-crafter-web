a7dfe27a49d41641d126458474b97a2775128e95
JoJoJux <80523427+J0J0HA@users.noreply.github.com>
2024-02-16 20:02:23 +0100

    Improved cfbuild.sh

--- cfbuild.sh
+++ cfbuild.sh
@@ -1,11 +1,9 @@
 npm i .
 
-npm i -g webpack webpack-cli
+npm i -g webpack webpack-cli sass
 
 if ($MODE == "development") then
-    echo("Building development version")
     npm run build-dev
 else
-    echo("Building production version")
     npm run build
 fi
